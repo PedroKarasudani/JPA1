05360012ec75342223ce62a9981453f3848aab70
Pedro K. L <pedro.karasudani@hotmail.com>
2023-09-19 15:18:50 -0300

    Relacionamento Many-to-many

---
+++ src/main/java/br/com/alura/loja/moledo/ItemPedido.java
@@ -0,0 +1,58 @@
+package br.com.alura.loja.moledo;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "itens_pedido")
+public class ItemPedido {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private BigDecimal precoUnitario;
+	private int quantidade;
+	
+	@ManyToOne
+	private Produto produto;
+	
+	@ManyToOne
+	private Pedido pedido;
+	
+	public ItemPedido() {
+	}
+
+	public ItemPedido(int quantidade, Produto produto, Pedido pedido) {
+		this.quantidade = quantidade;
+		this.produto = produto;
+		this.pedido = pedido;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public BigDecimal getPrecoUnitario() {
+		return precoUnitario;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+	
+	
+}
